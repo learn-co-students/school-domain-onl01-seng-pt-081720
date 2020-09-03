7de5b832456093c64c53985314fd0cd77d00c03b
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-03 11:31:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+     @name = name
+     @roster = Hash.new {|h,k| h [k] = []}
+     end 
+     def add_student(name,grade)
+     self.roster[grade] << name
+     end
+     def grade(grade)
+     self.roster[grade]
+     end
+     def sort 
+       self.roster.each do |grade,nu_array|
+         self.roster[grade] = nu_array.sort!
+        end
+     end
+  end 
+
+  
