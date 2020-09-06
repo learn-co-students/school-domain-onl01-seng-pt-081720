bbc2577a26f559effaa7299eceec6b27c0321e66
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-06 00:28:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  
+  
+  
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {} 
+  end 
+  
+  def roster 
+    @roster
+  end 
+  
+  def add_student(student, grade)
+   @student = student 
+   @grade = grade 
+   roster[grade] << name 
+  end 
+  
+end 
+
+
